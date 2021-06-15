1d11d16568bd4278c36f7f5763c596a9879cb2f1
Mykola Dorohyi <mdorohyi@c1r6p10.khpi.ucode-connect.study>
2021-06-15 21:36:12 +0300

    Fix rating for users

--- src/components/users/User.js
+++ src/components/users/User.js
@@ -1,8 +1,15 @@
 export default ({ id, login, fullName, profilePicture }) => {
   return (
     <div data-id={id} className='user-card'>
-      <div className='user-card-profile-picture'>
-        <img src={profilePicture} className='user-card-avatar' />
+      <div className='user-card-general'>
+        <div className='user-card-profile-picture'>
+          <img src={profilePicture} className='user-card-avatar' />
+        </div>
+        <div className='user-card-rating-container'>
+          <code className='user-card-rating'>
+            0
+          </code>
+        </div>
       </div>
       <div className='user-card-info'>
         <span className='user-card-login'>
@@ -11,9 +18,6 @@ export default ({ id, login, fullName, profilePicture }) => {
         <p className='user-card-full-name'>
           { fullName }
         </p>
-        <code className='user-card-rating'>
-          0
-        </code>
       </div>
     </div>
   )
