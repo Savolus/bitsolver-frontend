edb434d8571d6b2821ce125ead99f1af04b09944
Mykola Dorohyi <mdorohyi@c1r6p10.khpi.ucode-connect.study>
2021-06-15 19:36:33 +0300

    Lil style fixing

--- src/pages/Tags.js
+++ src/pages/Tags.js
@@ -15,7 +15,7 @@ export default () => {
   useEffect(async () => {
     setIsLoading(true)
 
-    const { data } = await axios.get(`http://bitsolver.herokuapp.com/api/categories?page=${page}&size=5`)
+    const { data } = await axios.get(`http://bitsolver.herokuapp.com/api/categories?page=${page}`)
   
     setTags(data)
     setIsLoading(false)
